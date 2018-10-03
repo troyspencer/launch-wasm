f15c787a7f727277509e9d22c6f20c25e0115923
Troy Spencer <troy.a.spencer@gmail.com>
2018-10-02 21:34:49 -0500

    Removed npm scripts in favor of gulp rebuild and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  exec = require('child_process').exec
+
+  
+// wasm watch
+gulp.task('wasm-watch', ['wasm-build'], function() {
+  setTimeout(function() { browserSync.reload(); }, 300); 
+})
+
+
+// wasm build
+gulp.task('wasm-build', function(cb) {
+  exec('env GOOS=js GOARCH=wasm go build -o server/build/main.wasm go/main.go', function(err, stdout, stderr) {
+    if (stdout) {
+      console.log(stdout)
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    cb(err)
+  })
+})
+
+
+// Static server
+gulp.task('browsersync', function() {
+  browserSync.init({
+    "callbacks": {
+      ready: function(err, bs) {
+        bs.utils.serveStatic.mime.define({ 'application/wasm': ['wasm'] });
+      }
+    },
+    server: {
+        baseDir: "./server"
+    },
+    "browser": "google chrome",
+    "open": false
+  });
+
+  gulp.watch("./go/*.go", ['wasm-watch'])
+});
+
+gulp.task('default', ['browsersync'])
