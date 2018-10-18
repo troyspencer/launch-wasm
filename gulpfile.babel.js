ecddaafaa791c2d444a6c0bbefd5dc5eb6bd60d9
troyspencer <troy.a.spencer@gmail.com>
2018-10-17 21:31:05 -0500

    Updated gulp to 4.0.0 and gulpfile syntax to match Added babel for es6 in gulp Updated browser-sync

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import { exec } from 'child_process';
+
+const browserSyncInstance = browserSync.create()
+
+const buildWasm = (cb) => {
+  exec('env GOOS=js GOARCH=wasm go build -o server/main.wasm go/main.go', (err, stdout, stderr) => {
+    if (stdout) {
+      console.log(stdout)
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    cb(err)
+    reloadServer()
+  })
+}
+
+const reloadServer = () => {
+  setTimeout(() => { browserSyncInstance.reload() }, 300); 
+}
+
+const serve = () => {
+  
+  browserSyncInstance.init({
+    "callbacks": {
+      ready: (err, bs) => {
+        bs.utils.serveStatic.mime.define({ 'application/wasm': ['wasm'] });
+      }
+    },
+    server: {
+        baseDir: "./server"
+    },
+    "browser": "google chrome",
+    "open": false
+  });
+
+  gulp.watch("./go/*.go", buildWasm)
+}
+
+const defaultTasks = gulp.series(serve)
+
+export {
+  buildWasm,
+  reloadServer,
+  serve
+}
+
+export default defaultTasks
