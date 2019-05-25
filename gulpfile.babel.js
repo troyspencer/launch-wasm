9dd610630cdad3c189ba48aabab80e24f2e6fc43
troyspencer <troy.a.spencer@gmail.com>
2019-05-24 22:49:10 -0500

    Renamed github.com/troyspencer/launch-wasm/go to github.com/troyspencer/launch-wasm/game

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,8 @@
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
 import { exec } from 'child_process';
 import workboxBuild from 'workbox-build';
 
-const browserSyncInstance = browserSync.create()
-
 const buildWasm = (cb) => {
-  exec('env GOOS=js GOARCH=wasm go build -o server/main.wasm go/main.go', (err, stdout, stderr) => {
-    if (stdout) {
-      console.log(stdout)
-    }
-    if (stderr) {
-      console.log(stderr);
-    }      
-    cb(err)
-    reloadServer()
-  })
-}
-
-const dockerBuildWasm = (cb) => {
   exec('rm -f server/static/main.wasm.gz && rm -f server/static/main.wasm && env GOOS=js GOARCH=wasm go build -o server/static/main.wasm go/main.go && gzip -k server/static/main.wasm', (err, stdout, stderr) => {
     if (stdout) {
       console.log(stdout)
@@ -79,34 +63,7 @@ const buildSW = () => {
 const watch = () => {
   var folders = ["","contact/","world/","bodies/"]
   for (var i = 0; i < folders.length; i++) {
-    gulp.watch("./go/"+folders[i]+"*.go", dockerBuildWasm)
-  }
-}
-
-
-const reloadServer = () => {
-  setTimeout(() => { browserSyncInstance.reload() }, 300); 
-}
-
-const serve = () => {
-  
-  browserSyncInstance.init({
-    "callbacks": {
-      ready: (err, bs) => {
-        bs.utils.serveStatic.mime.define({ 'application/wasm': ['wasm'] });
-      }
-    },
-    server: {
-        baseDir: "./server"
-    },
-    "browser": "google chrome",
-    "open": false
-  });
-
-  var folders = ["","contact/","world/"]
-
-  for (var i = 0; i < folders.length; i++) {
-    gulp.watch("./go/"+folders[i]+"*.go", buildWasm)
+    gulp.watch("./game/"+folders[i]+"*.go", buildWasm)
   }
 }
 
@@ -114,11 +71,8 @@ const defaultTasks = gulp.series(serve)
 
 export {
   buildWasm,
-  reloadServer,
-  serve,
-  watch,
-  dockerBuildWasm,
-  buildSW
+  buildSW,
+  watch
 }
 
 export default defaultTasks
