25b2310da0b3a9cf95582d31007b02367e50fb98
troyspencer <troy.a.spencer@gmail.com>
2019-06-09 17:54:47 -0500

    Settings menu improved

--- react/components/settingsSidebar.js
+++ react/components/settingsSidebar.js
@@ -3,6 +3,15 @@ import Sidebar from "react-sidebar";
 import SettingsButton from "./settingsButton"
 import Game from './game'
 import StatsToggle from "./statsToggle";
+import FlexView from "react-flexview/lib";
+import { Header } from "semantic-ui-react";
+
+const styles = {
+  header: {
+    color: "grey",
+    
+  }
+}
 
 export default class SettingsSidebar extends React.Component {
   constructor(props) {
@@ -34,13 +43,19 @@ export default class SettingsSidebar extends React.Component {
   }
 
   render() {
-    const sidebarContent = <StatsToggle onShowStatsChange={this.handleStatsChange} />
+    const sidebarContent = (
+      <FlexView column marginTop="10" vAlignContent='center'>
+        <Header style={styles.header} textAlign="center" content="Settings" />
+        <StatsToggle showStats={this.state.showStats} onShowStatsChange={this.handleStatsChange} />
+      </FlexView>
+    )
+    
     return (
       <Sidebar
         sidebar={sidebarContent}
         open={this.state.sidebarOpen}
         onSetOpen={this.onSetSidebarOpen}
-        styles={{ sidebar: { background: "#464646", color: "grey" } }}
+        styles={{ sidebar: { background: "#464646", color: "grey"} }}
       >
         <Game showStats={this.state.showStats} />
         <SettingsButton onClick={this.handleSidebarChange} />
--- react/components/statsToggle.js
+++ react/components/statsToggle.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Checkbox, Segment, Label, Divider, Header } from 'semantic-ui-react'
+import { Checkbox, Label } from 'semantic-ui-react'
+import FlexView from 'react-flexview'
 
 export default class StatsToggle extends React.Component {
     constructor(props) {
@@ -13,14 +14,14 @@ export default class StatsToggle extends React.Component {
 
     render(){
         return (
-            <Segment>
-                <Header as='h2' floated='right'>
-                Settings
-                </Header>
-                <Divider clearing />
-                <Checkbox toggle onChange={this.handleChange} />      
-                <Label color="grey" content="Show Stats" /> 
-            </Segment>
+            <FlexView vAlignContent="center" hAlignContent='center'>
+                <FlexView marginLeft="10" vAlignContent="center" hAlignContent='center'>
+                    <Label color="grey" content={this.props.showStats ? "Stats Visible" : "Stats Hidden"} />   
+                </FlexView>
+                <FlexView marginLeft="10" marginRight="10" vAlignContent="center" hAlignContent='center'>
+                    <Checkbox onChange={this.handleChange} />  
+                </FlexView>
+            </FlexView>  
         )
     }
     
