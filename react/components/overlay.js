18ee4f3ed87a89efdc575dc8eadb59fffab3e11c
troyspencer <troy.a.spencer@gmail.com>
2019-06-10 19:45:32 -0500

    overlay function component

--- react/components/overlay.js
+++ react/components/overlay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "react-sidebar";
 import SettingsButton from "./settingsButton"
 import Stats from "./stats";
@@ -6,52 +6,27 @@ import Game from './game'
 import SidebarContent from "./sidebarContent";
 import FlexView from 'react-flexview';
 
-export default class Overlay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarOpen: false,
-      showStats: true,
-    };
-    this.handleSidebarChange = this.handleSidebarChange.bind(this);
-    this.onSidebarChange = this.onSidebarChange.bind(this);
-    this.handleStatsChange = this.handleStatsChange.bind(this);
-    this.onShowStatsChange = this.onShowStatsChange.bind(this);
+export default function Overlay() {
+  const [showStats, setShowStats] = useState(true)
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebarOpen = () => {
+    setSidebarOpen(!sidebarOpen)
   }
 
-  handleSidebarChange() {
-    this.onSidebarChange(!this.state.sidebarOpen);
-  }
-
-  onSidebarChange(open) {
-    this.setState({ sidebarOpen: open });
-  }
-
-  handleStatsChange(show) {
-    this.onShowStatsChange(show)
-  }
-
-  onShowStatsChange(showStats) {
-    this.setState({showStats: showStats})
-  }
-
-  render() {
-    
-    return (
-      <Sidebar
-        sidebar={<SidebarContent showStats={this.state.showStats} onShowStatsChange={this.handleStatsChange} />}
-        docked={this.state.sidebarOpen}
-        open={this.state.sidebarOpen}
-        onSetOpen={this.onSidebarChange}
-        styles={{ sidebar: { background: "#464646", color: "grey"} }}
-      >
-        <Game />
-        <FlexView vAlignContent='top'>
-          <SettingsButton onClick={this.handleSidebarChange} />
-          <Stats showStats={this.state.showStats} />
-        </FlexView>
-        
-      </Sidebar>
-    );
-  }
+  return (
+    <Sidebar
+      sidebar={<SidebarContent showStats={showStats} onShowStatsChange={setShowStats} />}
+      docked={sidebarOpen}
+      open={sidebarOpen}
+      onSetOpen={setSidebarOpen}
+      styles={{ sidebar: { background: "#464646", color: "grey"} }}
+    >
+      <Game />
+      <FlexView vAlignContent='top'>
+        <SettingsButton onClick={toggleSidebarOpen} />
+        <Stats showStats={showStats} />
+      </FlexView>
+    </Sidebar>
+  );
 }
