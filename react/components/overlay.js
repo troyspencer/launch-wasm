2f0285fcd84430f61af7b47993b3a6f5f8c4eae1
troyspencer <troy.a.spencer@gmail.com>
2019-06-11 20:51:40 -0500

    loading is placed near center of the screen with correct styling

--- react/components/overlay.js
+++ react/components/overlay.js
@@ -14,11 +14,7 @@ export default function Overlay() {
 
   const styles = {
     spin: {
-      margin: 0,
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)"
+      marginTop: "50%",
     }
   }
 
